533247d09ada2f58f5c97fa745c3fb6a7b1dfa72
Joshua <data@shard.wtf>
2024-04-03 06:12:06 +0200

    fix leaderboard and prod dep

--- frontend/src/pages/api/leaderboard.ts
+++ frontend/src/pages/api/leaderboard.ts
@@ -1,12 +1,10 @@
-import conn from "@/lib/db";
+import sql from "@/lib/sql";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const query =
-      "select q.id, q.type, q.question, q.difficulty, q.correct_answer, q.wrong_answers, c.category from questions q left join category c on q.category = c.id";
-    const result = await conn.query(query);
-    return res.status(200).json(result.rows);
+    const result = await sql`"select q.id, q.type, q.question, q.difficulty, q.correct_answer, q.wrong_answers, c.category from questions q left join category c on q.category = c.id";`
+    return res.status(200).json(result);
   } catch (error) {
     console.log(error);
   }
